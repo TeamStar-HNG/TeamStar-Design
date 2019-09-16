c43052a22ba29903317a8c28d69491a2d2ee483f
iPeluwa <ipeluwa@gmail.com>
2019-09-16 22:02:24 +0100

    login script added

--- index.php
+++ index.php
@@ -21,16 +21,40 @@
                 <img src="https://res.cloudinary.com/partypack/image/upload/c_scale,w_181/v1568628007/facebook_ylbio1.jpg" alt="facebook-logo"/>
                 <img src="https://res.cloudinary.com/partypack/image/upload/c_scale,w_181/v1568628007/facebook_ylbio1.jpg" alt="twitter-logo"/>                
         </div>
-
-        <form action= "/visitor-login-form" target="_blank" method="post" onsubmit= "return validateLogin()">
+<?php
+require('db.php');
+session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['email'])){
+        // removes backslashes
+ $email = stripslashes($_REQUEST['email']);
+        //escapes special characters in a string
+ $email = mysqli_real_escape_string($con,$email);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($con,$password);
+ //Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE email='$email'
+and password='".md5($password)."'";
+ $result = mysqli_query($con,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+        if($rows==1){
+     $_SESSION['email'] = $email;
+            // Redirect user to index.php
+     header("Location: index.php");
+         }else{
+ echo "<h3>Email/password is incorrect.</h3>";
+ }
+    }else{
+?>
+        <form action= "" target="_blank" method="post" onsubmit= "return validateLogin()">
                 <p>-OR-</p>                
                 <label for="email-label" id="email">Email Address</label>
                 <br>
-                <input type="text" id="email-label" name="email-add" placeholder="Enter valid email"  required>
+                <input type="text" id="email-label" name="email" placeholder="Enter valid email"  required>
                 <br><br><br>  
                 <label for="password-label" id="password">Password</label>
                 <br>
-                <input type="text" id="password-label" name="password_name" placeholder="********************" minlength="8" required>
+                <input type="text" id="password-label" name="password" placeholder="********************" minlength="8" required>
                 <br><br>
 
             <div class="checkbox">                 
@@ -58,7 +82,8 @@
         </div>
     </div>
 
-</div>    
+</div>
+<?php } ?>
 </body>
 </html>
 
