9f2c61c8e4f29526aee0c958b909e82d58bcce40
Nedu Robert <nedumrobert@gmail.com>
2019-09-19 16:59:49 +0100

    feat: updated authentication tab

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+$('#goToRegister').click(function() {
+    $('#nav-tab a[href="#nav-register"]').tab('show');
+});
+
+$('#goToLogin').click(function() {
+    $('#nav-tab a[href="#nav-login"]').tab('show');
+});
